f06562da89d145179ff9105bbe17c39362ccb0d0
Macbook <macbook@MacBook-Air-de-Macbook.local>
2015-05-09 15:12:44 -0500

    commit

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ControlRutas
+//
+//  Created by Macbook on 9/05/15.
+//  Copyright (c) 2015 Macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ControlRutas
+//
+//  Created by Macbook on 9/05/15.
+//  Copyright (c) 2015 Macbook. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
